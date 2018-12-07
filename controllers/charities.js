2a6f461efe7523ad461603688253e3b5b52ddcfb
Marianna <mckd2018@gmail.com>
2018-12-07 11:48:07 -0800

    add show route for charities

--- controllers/charities.js
+++ controllers/charities.js
@@ -1,17 +1,19 @@
 // controllers/charities.js
 const https = require('https');
-const Donation = require('../models/Charity');
+const Charity = require('../models/charity');
 
 module.exports = function(app) {
+    let url = 'https://api.data.charitynavigator.org/v2/Organizations?app_id=246b4b41&app_key=b45841a7b4baf09d703c4f717a6d4927&pageSize=3'
+
     //ROOT ROUTE INDEX
     app.get('/charities', function (req, res) {
         var queryString = req.query.term;
         // Encode the query string to remove white spaces and restricted characters
         var term = encodeURIComponent(queryString);
         // Put the search term into the giphy API search URL
-        var url = 'https://api.data.charitynavigator.org/v2/Organizations?app_id=246b4b41&app_key=b45841a7b4baf09d703c4f717a6d4927&pageSize=3&search='+ term
-
-        https.get(url, function(response) {
+        // var url = 'https://api.data.charitynavigator.org/v2/Organizations?app_id=246b4b41&app_key=b45841a7b4baf09d703c4f717a6d4927&pageSize=3&search='+ term
+// &search='+ term
+        https.get(url + `&search=` + term, function(response) {
             // Set encoding of response to utf8
             // READ ARTICLE ABOUT THIS TONIGHT
             response.setEncoding('utf8');
@@ -27,24 +29,31 @@ module.exports = function(app) {
                 // when data is fully received parse into JSON
                 var parsed = JSON.parse(body);
                 // Render the home template and pass the charity data into the template
-                console.log(parsed.charityNavigatorURL)
-                // => RETURN JSON
-                if (req.header('Content-Type') == 'application/json') {return res.send({ orgs: parsed});}
+                // console.log(`Charity Navigator URLs${{orgs: parsed}}`)
+                // for each org in orgs array
+                function charityNameToLocalModel(orgs) {
+                    orgs.forEach(function(charity) {
+                        // console.log(` Charity ein number: ${charity.charityName}`);
+                        // create new instance of model
+                        // add property charity.name to model
+                        Charity.create(charity)
+                    });
+                }
 
+                charityNameToLocalModel(parsed);
+
+                // => RETURN JSON
+                if (req.header('Content-Type') == 'application/json') {return res.json({ orgs: parsed});}
+                // return res.json(parsed)
                 //=> RETURN HTML
                 return res.render('charities-index', {orgs: parsed}); //=> RENDER A TEMPLATE
             });
         });
     })
-
-    // Not operational yet
+// Very helpful: https://github.com/jayceazua/iMakeTunesMovie/blob/master/server.js
     // SHOW
-    app.get('/charities/ein', function (req, res) {
-        var queryString = req.query.term;
-        // Encode the query string to remove white spaces and restricted characters
-        var term = encodeURIComponent(queryString);
-        // Put the search term into the giphy API search URL
-        var url = 'https://api.data.charitynavigator.org/v2/Organizations?app_id=246b4b41&app_key=b45841a7b4baf09d703c4f717a6d4927&pageSize=3&search='+ term
+    app.get('/charities/:id', (req, res) => {
+        const charityId = req.params.id;
 
         https.get(url, function(response) {
             // Set encoding of response to utf8
@@ -60,15 +69,29 @@ module.exports = function(app) {
 
             response.on('end', function() {
                 // when data is fully received parse into JSON
-                var parsed = JSON.parse(body);
-                // Render the home template and pass the charity data into the template
-                // console.log(parsed)
-                // => RETURN JSON
-                if (req.header('Content-Type') == 'application/json') {return res.send({ orgs: parsed});}
+                var parsed = JSON.parse(body)[charityId];
 
+                // => RETURN JSON
+                if (req.header('Content-Type') == 'application/json') {return res.json({ orgs: parsed});}
+                // return res.json(parsed)
                 //=> RETURN HTML
-                return res.render('charities-index', {orgs: parsed}); //=> RENDER A TEMPLATE
+                // return res.json(parsed)
+                return res.render('charities-show', {orgs: parsed}); //=> RENDER A TEMPLATE
             });
         });
+
     })
+    // app.get('/charities/:id', function (req, res) {
+    //     // find charity
+    //     Charity.findById(req.params.id).then((charity) => {
+    //         // fetch its comment
+    //         //Comment.find({ donationId: req.params.id }).then(comments => {
+    //             //respond with the template with both values
+    //         res.render('charities-show', { charity: charity})
+    //         //})
+    //     }).catch((err) => {
+    //         // catch errors
+    //         console.log(err.message);
+    //     });
+    // });
 }
--- models/charity.js
+++ models/charity.js
@@ -3,10 +3,15 @@ Model/Data Layer - where you put the code dedicated to interacting with the data
 models/Donation.js
 */
 /* Object Document Mapper */
+// Check if your database is created and getting the right data
+// https://robomongo.org/download
 const mongoose = require('mongoose');
 
 const Charity = mongoose.model('Charity', {
-    ein: String
+    charityName: String,
+    mission: String,
+    mailingAddress: Object,
+    irsClassification: Object
 })
 
 module.exports = Charity;
